e45131361b97aee61f7674c4befb8597df0ec3f8
Kevin Washington <kevw12188@gmail.com>
2014-09-26 22:28:34 -0400

    tranclusion in directive

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var app = angular.module('app', [])
 
+app.directive('transcludeExample', function(){
+	return{
+		restrict: 'E', 
+		transclude: true,
+		template: '<div>This is content from the directive. <ng-transclude></ng-transclude></div>',
+		link: function(scope, element, attrs){
+			console.log(scope);
+		}
+	}
+})
+
+
+
+
+
+
